041716237ad83237b8376c67728f55b93463f8ec
Beka Modebadze <bexx.modd@gmail.com>
2023-12-02 12:37:26 -0800

    day1 part 2 solution

--- aoc2023/src/main.rs
+++ aoc2023/src/main.rs
@@ -1,7 +1,11 @@
-mod day1;
+mod day1_part1;
+mod day1_part2;
 
 fn main() {
   env_logger::init();
-  let res = day1::solve();
-  println!("<<<=== Total calibration value = [{res}] ===>>>");
+  let res1 = day1_part1::solve();
+  println!("<<<=== Total calibration value Part 1 = [{res1}] ===>>>");
+
+  let res2 = day1_part2::solve();
+  println!("<<<=== Total calibration value Part 2 = [{res2}] ===>>>");
 }
